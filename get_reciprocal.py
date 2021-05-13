399fabc04c4818885eac047a51249bd80990ca9f
Bethany Moore <john3784@msu.edu>
2021-05-13 10:15:17 -0500

    update reciprocal.py

--- get_reciprocal.py
+++ get_reciprocal.py
@@ -6,7 +6,9 @@ def get_recipricol(blast,idenT,expcT):
     BMQ= {}
     BMS= {}
     while inl != "":
-        L = inl.split("/t")
+        L = inl.strip().split("\t")
+        #print(len(L))
+        #print(L)
         q = L[0]            #query
         s = L[1]            #subject
         p = float(L[2])     #percent identity
@@ -15,18 +17,19 @@ def get_recipricol(blast,idenT,expcT):
         if p >= idenT and E < expcT:  #p and E filters
         	# if original e-val is higher than new, replace
             if q not in BMQ or BMQ[q][1] > E:  #update BMQ if q does not exist
-                BMQ = [s,E]                   # or original e higher than new e
+                BMQ[q] = [s,E]                   # or original e higher than new e
             # Do the same for BMS[s]
             if s not in BMS or BMS[s][1] > E:
-                BMS = [q,E]
+                BMS[s] = [q,E]
                 
         inl = inp.readline()
     #now check for reciprocal best match and output
     output = open(blast+".recipricol","w")
-    for q in BMQ:
+    print(len(BMQ.keys()),len(BMS.keys()))
+    for q in BMQ.keys():
         s = BMQ[q][0]
-        if s in BMS:
-            if q == BMS[s]:
+        if s in BMS.keys():
+            if q == BMS[s][0]:
                 output.write("%s\t%s\n" % (q,s))
     inp.close()
     output.close()
